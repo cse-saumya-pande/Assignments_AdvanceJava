f27ca6551b44480b753a9c185f490c71f3b7ad6c
cse-saumya-pande <cse.saumya.pande@gmail.com>
2026-02-20 14:26:40 +0530

    One to Many Mapping

---
+++ oneToMany/src/main/java/oneToMany/Customer.java
@@ -0,0 +1,62 @@
+package oneToMany;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "customer_id")
+	private List<Account> account;
+
+	public Customer() {
+		super();
+	}
+
+	public Customer(String name, List<Account> account) {
+		super();
+		this.name = name;
+		this.account = account;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Account> getAccount() {
+		return account;
+	}
+
+	public void setAccount(List<Account> account) {
+		this.account = account;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", account=" + account + "]";
+	}
+}
