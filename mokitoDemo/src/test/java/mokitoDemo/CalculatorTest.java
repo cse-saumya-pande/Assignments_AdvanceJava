18581a0fe466713110cd819afd726ae0dad4e5f2
cse-saumya-pande <cse.saumya.pande@gmail.com>
2026-02-21 12:00:59 +0530

    Update CalculatorTest.java

--- mokitoDemo/src/test/java/mokitoDemo/CalculatorTest.java
+++ mokitoDemo/src/test/java/mokitoDemo/CalculatorTest.java
@@ -29,38 +29,3 @@ public class CalculatorTest {
 	
 }
 
-1. list.stream()
-.sorted(Comparator.reverseOrder())
-.skip(1)
-.limit(1);
-
-2. list.stream()
-.sorted(Comparator.comparing(Employee::getSal).reversed())
-.limit(3);
-
-3. list.stream()
-.collect(Collectors.groupingBy(String::length,
-		Collectors.counting())
-		);
-
-4. list.stream()
-.collect(Collectors.toMap(Employee::getId, Employee::getName
-		(existing, replacement) -> existing));
-
-5. list.stream()
-.filter(s -> s!= null)
-.filter(s.matches("^[A]"))
-.sorted(Comparator.reverseOrder())
-.collect(Collectors.toList()); // 
-//Corrected
-list.stream()
- .filter(Objects::nonNull)
- .filter(s -> s.startsWith("A"))
- .sorted(Comparator.reverseOrder())
- .collect(Collectors.toList());
-
-
-6. list.stream()
-.collect(Collectors.toMap())
-
-5. 
