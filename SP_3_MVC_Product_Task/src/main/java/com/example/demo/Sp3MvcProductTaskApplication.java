bc3b315fc281c1b874cef722608488338db7da75
cse-saumya-pande <cse.saumya.pande@gmail.com>
2026-02-23 15:24:32 +0530

    Product Task MVC

---
+++ SP_3_MVC_Product_Task/src/main/java/com/example/demo/Sp3MvcProductTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp3MvcProductTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp3MvcProductTaskApplication.class, args);
+	}
+
+}
