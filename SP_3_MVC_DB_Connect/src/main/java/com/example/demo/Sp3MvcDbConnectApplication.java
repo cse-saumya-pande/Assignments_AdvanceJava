2a9a103ef30998436a4fe64412e31e4d5b4de54a
cse-saumya-pande <cse.saumya.pande@gmail.com>
2026-02-25 12:09:50 +0530

    connection with DB

---
+++ SP_3_MVC_DB_Connect/src/main/java/com/example/demo/Sp3MvcDbConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp3MvcDbConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp3MvcDbConnectApplication.class, args);
+	}
+
+}
---
+++ SP_3_MVC_DB_CustomerManagementSystem/src/main/java/com/example/demo/Sp3MvcDbCustomerManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp3MvcDbCustomerManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp3MvcDbCustomerManagementSystemApplication.class, args);
+	}
+}
